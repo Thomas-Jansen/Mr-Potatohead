cbb4119c0dd6a9fb099c69e0273e216d3e96c9c7
Thomas-Jansen <thomasjansen010@gmail.com>
2018-02-06 10:49:26 +0100

    Mr Potatohead

---
+++ app/src/main/java/jansen/thomas/mrpotatohead/MainActivity.java
@@ -0,0 +1,20 @@
+package jansen.thomas.mrpotatohead;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void checkClicked(View v){
+        Log.d("potato", "Checkclicked");
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
