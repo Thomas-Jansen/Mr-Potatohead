0dcbd3d7e10801094ab239738271597b4e951dd5
Thomas-Jansen <thomasjansen010@gmail.com>
2018-02-07 12:28:17 +0100

    Mr Potatohead

--- app/src/main/java/jansen/thomas/mrpotatohead/MainActivity.java
+++ app/src/main/java/jansen/thomas/mrpotatohead/MainActivity.java
@@ -1,14 +1,29 @@
 package jansen.thomas.mrpotatohead;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    public void checkClicked(View v){
-        Log.d("potato", "Checkclicked");
+
+
+    public void checkClicked(View v) {
+        CheckBox checkbox = (CheckBox) v;
+        String bodypart = checkbox.getText().toString();
+        int bodypartID = getResources().getIdentifier("image" + bodypart, "id", getPackageName());
+        //bovenstaande methode gevonden op
+        // https://stackoverflow.com/questions/6679434/android-findviewbyid-with-a-variant-string/6679476
+        ImageView image = (ImageView) findViewById(bodypartID);
+
+        if (image.isShown())
+            image.setVisibility(View.INVISIBLE);
+        else
+            image.setVisibility(View.VISIBLE);
+
     }
 
 
